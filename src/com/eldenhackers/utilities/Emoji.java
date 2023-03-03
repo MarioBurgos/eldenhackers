856d1a7a3061bffe6a55dd21eae879892a31e65a
Mario Burgos <mario.burpi@gmail.com>
2023-03-03 10:51:40 +0100

    Added emojis

--- src/com/eldenhackers/utilities/Emoji.java
+++ src/com/eldenhackers/utilities/Emoji.java
@@ -1,2 +1,12 @@
-package com.eldenhackers.utilities;public class Emoji {
+package com.eldenhackers.utilities;
+
+public class Emoji {
+
+    public final static String DAGGER = "\uD83D\uDDE1️";
+    public final static String PUNCH = "\uD83D\uDC4A️";
+    public final static String WARRIOR_CONCENTRATION = "\uD83D\uDCA2️";
+    public final static String FIREBALL = "\uD83D\uDD25️";
+    public final static String STAFF = "\uD83E\uDE84️";
+    public final static String WIZARD_CONCENTRATION = "✨️";
+    public final static String SKULL = "💀";
 }
