febdfb445eb7f6f8dd0bce6d14911f3e731e5a0e
Mario Burgos <mario.burpi@gmail.com>
2023-03-03 16:36:26 +0100

    changed STAFF_HIT emoji

--- src/com/eldenhackers/utilities/Emoji.java
+++ src/com/eldenhackers/utilities/Emoji.java
@@ -7,7 +7,7 @@ public class Emoji {
     public final static String PUNCH = "\uD83D\uDC4A️";
     public final static String WARRIOR_CONCENTRATION = "\uD83D\uDCA2️";
     public final static String FIREBALL = "\uD83D\uDD25️";
-    public final static String STAFF = "\uD83E\uDE84️";
+    public final static String STAFF = "\uD83E\uDDAF️";
     public final static String WIZARD_CONCENTRATION = "✨️";
     public final static String SKULL = "💀";
 }
