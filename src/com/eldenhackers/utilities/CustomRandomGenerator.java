4c76b11d54d482c46036e57649b206667d1f9241
Mario Burgos <mario.burpi@gmail.com>
2023-03-03 16:37:58 +0100

    CustomRandomGenerator works better

--- src/com/eldenhackers/utilities/CustomRandomGenerator.java
+++ src/com/eldenhackers/utilities/CustomRandomGenerator.java
@@ -1,2 +1,11 @@
-package com.eldenhackers.utilities;public class CustomRandomGenerator {
+package com.eldenhackers.utilities;
+
+import java.util.Random;
+
+public class CustomRandomGenerator {
+
+    public static Integer getRangedInt(int low, int high){
+        Random r = new Random();
+        return r.nextInt(high - low ) +low;
+    }
 }
